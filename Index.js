71da6721ad9450c2c0c3d96b244393edce36d636
Blake Robert <Blaker.blaker4@gmail.com>
2018-09-23 01:28:37 -0400

    fully functional application

--- Index.js
+++ Index.js
@@ -1,2 +1,53 @@
 var Word = require('./Word.js');
 var inquirer = require('inquirer');
+var lives;
+var theWord;
+
+function initialize() {
+    lives =10;
+    var wordOptions = ["ahoy", "landlubber", "booty", "buccaneer", "cutlass", "doubloons", "grog", "plunder", "rum",]
+    var num = Math.floor((Math.random() * wordOptions.length));
+    secretWord = wordOptions[num];
+    theWord = new Word();
+    theWord.populateArray(secretWord);
+    theWord.wordString();
+    prompt();
+}
+function prompt() {
+    console.log('REMAINING GUESSES: ' + lives);
+    inquirer
+        .prompt([{
+            type: "input",
+            name: "guess",
+            message: "GUESS A LETTER",
+            validate: function (value) {
+                if (value.length > 1) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }])
+        .then(function (answer) {
+            theWord.guess(answer.guess);
+            if (theWord.currentWord.toString("") === theWord.secretWord.toString("")) {
+                console.log("You Guessed correctly\nNew word")
+                initialize();
+            } else {
+                if (theWord.oldWord.toString("") === theWord.currentWord.toString("")) {
+                    lives--;
+                    if (lives === 0) {
+                        console.log(`
+~~~~~~~~~~~~~~~~~~GAME OVER~~~~~~~~~~~~~~~~~~~~~
+`)
+                    } else {
+                        prompt();
+                    }
+                }else{
+                    prompt();
+                }
+            }
+        });
+}
+
+initialize();
--- Word.js
+++ Word.js
@@ -1,17 +1,25 @@
 var Letter = require('./Letter.js');
 
 function Word(){
+    this.secretWord =[];
     this.array =[];
+    this.currentWord =[];
+    this.oldWord =[];
     this.wordString = function(){
         var wordArray =[];
-        for(i=0;i<this.array.length;i++){
+        this.oldWord = this.currentWord;
+        this.currentWord =[];
+        for(let i=0;i<this.array.length;i++){
            var letter = this.array[i].toString();
            wordArray.push(letter);
+           this.currentWord.push(letter);
         }
         console.log(wordArray.join(' '))
     };
     this.populateArray = function(word){
         for(i=0;i<word.length;i++){
+            var char = word.charAt(i);
+            this.secretWord.push(char.toUpperCase());
             var character = new Letter (word.charAt(i));
             this.array.push(character);
         }
@@ -20,13 +28,15 @@ function Word(){
         for(var i=0; i<this.array.length;i++){
         this.array[i].doesItMatch(userinput);
         }
+        this.wordString();
     };
 }
+module.exports = Word;
 
-var testWord = new Word();
-var testInput = process.argv.slice(2).join(' ');
-var userInput = 'o';
-testWord.populateArray(testInput);
-testWord.guess(userInput);
-testWord.wordString();
+// var testWord = new Word();
+// var testInput = process.argv.slice(2).join(' ');
+// var userInput = 'o';
+// testWord.populateArray(testInput);
+// testWord.guess(userInput);
+// testWord.wordString();
 
